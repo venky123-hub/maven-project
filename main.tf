6734983cfb7b3d81f0b91e76002fb4b183c54f5b
venky123-hub <70683807+venky123-hub@users.noreply.github.com>
2020-09-14 18:49:37 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,39 @@
+provider "aws" {
+
+    region = "us-east-1"
+      
+}
+
+resource "aws_key_pair" "main" {
+    key_name      = "publickey"
+  public_key      = file(var.public_key_file)
+
+}
+resource "aws_instance" "k8Master"{
+   ami                              = "ami-0758470213bdd23b1"
+   instance_type                    = var.master_instance_type
+   vpc_security_group_ids           =  ["sg-0a90573d6af271603"]
+   key_name                         = aws_key_pair.main.key_name
+   associate_public_ip_address      = true
+   tags                             = {
+       Name                         = "master"
+   }
+   provisioner "local-exec" {
+           command = "echo [master] '\n' ${aws_instance.k8Master.public_ip}|tee >> inventory"
+
+        }
+}
+
+resource "aws_instance" "k8Worker"{
+   ami                              = "ami-0758470213bdd23b1"
+   instance_type                    = var.worker_instance_type
+   vpc_security_group_ids           =  ["sg-0a90573d6af271603"]
+   key_name                         = aws_key_pair.main.key_name
+   associate_public_ip_address      = true
+   tags                             = {
+       Name                         = "worker"
+   }
+   provisioner "local-exec" {
+           command = "echo  [worker] '\n' ${aws_instance.k8Worker.public_ip}|tee >> inventory"
+        }
+}
